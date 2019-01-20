fa7e0693869c298a035f7f7c848655ed51e0ef3e
Patrick Capps <patrick.capps0@gmail.com>
2019-01-19 21:10:38 -0500

    made a brain

--- Assignment1/D3_Vis/vis1.js
+++ Assignment1/D3_Vis/vis1.js
@@ -1,13 +1,25 @@
-var data = [];
+var dataRows = 182;
+var dataColumns = 218;
+var width = "850px";
+var height = "850px";
 
-// Load data from txt file and parse into 2 dimensional array
 d3.text("data/dataset1.txt").then(function(text) {
   var ssv = d3.dsvFormat(" ");  // Create space separated value parser
   ssv.parseRows(text, function(d, i) {
-    data[i] = d.map(function(value) {
-      return +value;
-    });
+    for (var j = 0; j < d.length; j++) {
+      var value = +d[j];
+      if (value > 0) {
+        svg.append("circle")
+          .attr("cx", i * 4)
+          .attr("cy", j * 4)
+          .attr("r", value * 15)
+          .style("fill", "blue");
+      }
+    }
   });
 });
 
-console.log(data);
+var svg = d3.select("#vis1")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);

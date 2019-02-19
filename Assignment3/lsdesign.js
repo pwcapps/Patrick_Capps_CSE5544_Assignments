7b5a8fe729d96a6051413fe252f9d40b1115e824
Patrick Capps <patrick.capps0@gmail.com>
2019-02-19 00:07:44 -0500

    color attempt

--- Assignment3/lsdesign.js
+++ Assignment3/lsdesign.js
@@ -1,1 +1,18 @@
-d3.select("body").style("background-color", "black");
+d3.select("body").style("background-color", "gray");
+
+// D3 Color scale
+var c20c = d3.scale.category20c();
+
+var svg1 = d3.select("body")
+             .append("svg")
+             .attr("width", 400)
+             .attr("height", 50);
+
+svg4.selectAll("circle")
+   .data( d3.range(20) )
+   .enter()
+   .append("circle")
+   .attr("r", 9 )
+   .attr("cx", d3.scale.linear().domain([-1, 20]).range([0, 400]) )
+   .attr("cy", 10)
+   .attr("fill", c20c );

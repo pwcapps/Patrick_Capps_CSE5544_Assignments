c7a1f2b690a6d4ffc7e9120e1ca4c4483f848167
Patrick Capps <patrick.capps0@gmail.com>
2019-02-19 10:49:15 -0500

    working color scale; new svg with y-axis

--- Assignment3/lsdesign.js
+++ Assignment3/lsdesign.js
@@ -8,7 +8,7 @@ var svg1 = d3.select("body")
              .attr("width", 400)
              .attr("height", 50);
 
-svg4.selectAll("circle")
+svg1.selectAll("circle")
    .data( d3.range(20) )
    .enter()
    .append("circle")
@@ -16,3 +16,21 @@ svg4.selectAll("circle")
    .attr("cx", d3.scale.linear().domain([-1, 20]).range([0, 400]) )
    .attr("cy", 10)
    .attr("fill", c20c );
+
+var numPatients = 42;
+var width = 800, height = 800;
+
+
+// Generate svg with y-axis in middle to mark TBI location
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+svg.append("line")
+  .attr("x1", width / 2)
+  .attr("y1", 0)
+  .attr("x2", width / 2)
+  .attr("y2", height)
+  .style("stroke", "rgb(255,255,255)")
+  .style("stroke-width", 3);
